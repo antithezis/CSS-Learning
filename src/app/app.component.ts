d88ff8fce66dfc2399394f8750ce48acba555aff
antithezis <juandbm2001@gmail.com>
2024-02-23 18:59:15 -0500

    Update app component and add gallery component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './basics/flexbox/landing-page/landingPage.component';
+import { GalleryComponent } from './basics/flexbox/gallery/gallery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
