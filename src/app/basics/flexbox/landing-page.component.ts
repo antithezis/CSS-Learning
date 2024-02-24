3cf672ad41aecc05dd5eda694453f4f5296d0373
antithezis <juandbm2001@gmail.com>
2024-02-24 16:42:00 -0500

    Change name of landing-page.component

--- src/app/basics/flexbox/landing-page.component.ts
+++ src/app/basics/flexbox/landing-page.component.ts
@@ -4,8 +4,38 @@ import { Component } from '@angular/core';
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
-  styles: [
-    `
+  template: `
+    <header class="hero">
+      <nav class="nav">
+        <section class="nav__container container">
+          <figure class="nav__logo">
+            <h2 class="nav__title">Antithezis</h2>
+          </figure>
+
+          <div class="nav__links">
+            <a href="#" class="nav__link">Inicio</a>
+            <a href="#" class="nav__link">Acerca de</a>
+            <a href="#" class="nav__link">Contacto</a>
+          </div>
+        </section>
+      </nav>
+
+      <section class="hero__main container">
+        <div class="hero__texts">
+          <h1 class="hero__title">Bienvenido a Antithezis Design</h1>
+          <p class="hero__paragraph">
+            Aprende diseño web moderno, con nuestros cursos gratis o premium
+          </p>
+          <a href="#" class="cta">Aprende Ahora</a>
+        </div>
+
+        <figure class="hero__picture">
+          <img src="assets/flexbox/flexboximg.svg" class="hero__img" />
+        </figure>
+      </section>
+    </header>
+  `,
+  styles: `
       .container {
         width: 90%;
         max-width: 1200px;
@@ -112,37 +142,5 @@ import { Component } from '@angular/core';
         }
       }
     `,
-  ],
-  template: `
-    <header class="hero">
-      <nav class="nav">
-        <section class="nav__container container">
-          <figure class="nav__logo">
-            <h2 class="nav__title">Antithezis</h2>
-          </figure>
-
-          <div class="nav__links">
-            <a href="#" class="nav__link">Inicio</a>
-            <a href="#" class="nav__link">Acerca de</a>
-            <a href="#" class="nav__link">Contacto</a>
-          </div>
-        </section>
-      </nav>
-
-      <section class="hero__main container">
-        <div class="hero__texts">
-          <h1 class="hero__title">Bienvenido a Antithezis Design</h1>
-          <p class="hero__paragraph">
-            Aprende diseño web moderno, con nuestros cursos gratis o premium
-          </p>
-          <a href="#" class="cta">Aprende Ahora</a>
-        </div>
-
-        <figure class="hero__picture">
-          <img src="assets/flexbox/flexboximg.svg" class="hero__img" />
-        </figure>
-      </section>
-    </header>
-  `,
 })
 export class LandingPageComponent {}
