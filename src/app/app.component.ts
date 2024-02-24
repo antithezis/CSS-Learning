ebe99bb878830f64d7e44b82593df175f6822eb4
antithezis <juandbm2001@gmail.com>
2024-02-23 19:44:19 -0500

    Update app component and add cards component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './basics/flexbox/landing-page/landingPage.component';
 import { GalleryComponent } from './basics/flexbox/gallery/gallery.component';
+import { CardsComponent } from './basics/flexbox/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, GalleryComponent],
+  imports: [RouterOutlet, LandingPageComponent, GalleryComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/basics/flexbox/cards/cards.component.ts
+++ src/app/basics/flexbox/cards/cards.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './cards.component.html',
-  styles: ``
+  styleUrl: './cards.component.css'
 })
 export class CardsComponent {
 
